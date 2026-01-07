ae036ab7f7d6036a6e40153e395933d7a699f775
arachi04 <arachi910@gmail.com>
2026-01-06 22:26:10 -0600

    Initial Spring Boot project with Hello world endpoint

---
+++ src/test/java/com/ejemplo/mi_api/MiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.mi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
